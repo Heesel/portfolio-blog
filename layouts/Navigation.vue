12cccdc1bf7b38451a1b61b72372c8601676c1dd
Heesel <hesselpa@live.nl>
2021-12-22 00:05:46 +0100

    Setup

---
+++ layouts/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <header class="text-gray-600 body-font">
+            <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                    <span class="ml-3 text-xl">HesselP</span>
+                </a>
+                <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                    <nuxt-link to="/" class="mr-5 text-gray-600 hover:text-gray-900 border-b border-gray-300 hover:border-gray-900 pb-1">Blog</nuxt-link>
+                    <nuxt-link to="/" class="mr-5 text-gray-600 hover:text-gray-900 border-b border-gray-300 hover:border-gray-900 pb-1">About me</nuxt-link>
+                </nav>
+            </div>
+        </header>
+    </div>
+</template>
+<script>
+  export default {
+
+  }
+</script>
